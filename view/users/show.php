7c60cad5627aac3427a79002ef48fdedd27e1224
Brais Domínguez Álvarez <braisda@gmail.com>
2018-04-23 19:07:52 +0200

    Modificada Vista Listar Usuarios
    Se listan todos los usuarios juntos sean del tipo que sean

--- view/users/show.php
+++ view/users/show.php
@@ -20,37 +20,51 @@ $view->setVariable ("title", "Show Users");
 
 <div id="container" class="container">
   <div id="background_title">
-    <h4 id="view_title"><?= i18n("Users List") ?></h4><a href="index.php?controller=users&amp;action=add"> <span class="oi oi-plus"></a>
+    <h4 id="view_title"><?= i18n("Users List") ?></h4><a href="index.php?controller=users&amp;action=search"> <span id="search_icon" class="oi oi-magnifying-glass"></a><a href="index.php?controller=users&amp;action=add"> <span class="oi oi-plus"></a>
   </div>
   <div class="row justify-content-around">
 
-    <div id="background_table" class="col-4">
-      <h4 id="table_title" ><?= i18n("Administrators") ?></h4>
+    <div id="background_table" class="col-9">
+      <br/>
       <div class="table-responsive">
             <table id="table_color" class="table table-sm table-dark">
               <thead class="thead-dark">
                 <tr>
                   <th scope="col">#</th>
+                  <th><?=i18n("Dni")?></th>
                   <th><?=i18n("Surname")?></th>
                   <th><?=i18n("Name")?></th>
+                  <th><?=i18n("Type")?></th>
                   <th><?=i18n("Operaciones")?></th>
                 </tr>
               </thead>
               <tbody>
                 <?php $n=1; foreach ($users as $user): ?>
-                  <?php if ($user->getIs_administrator() == 1): ?>
 
         						<tr>
                       <td><?= $n++ ?></td>
+                      <td><?= $user->getDni() ?></td>
         							<td><?= $user->getSurname() ?></td>
         							<td><?= $user->getName() ?></td>
+                      <?php
+                        $exp = "[\s]";
+                        $roles = preg_split($exp, $user->getType());
+                      ?>
                       <td>
-                        <a href="index.php?controller=users&amp;action=view&amp;id_user=<?= $user->getId_user() ?>"><span class="oi oi-magnifying-glass"></span></a>
+
+                        <?php
+
+                          foreach ($roles as $rol): ?>
+                            <?= i18n(ucwords($rol)) ?>
+                          <?php endforeach; ?>
+
+                      </td>
+                      <td>
+                        <a href="index.php?controller=users&amp;action=view&amp;id_user=<?= $user->getId_user() ?>"><span class="oi oi-zoom-in"></span></span></a>
                         <a href="index.php?controller=users&amp;action=update&amp;id_user=<?= $user->getId_user() ?>"><span class="oi oi-loop"></span></a>
                         <a href="index.php?controller=users&amp;action=delete&amp;id_user=<?= $user->getId_user() ?>"><span class="oi oi-trash"></span></a>
                       </td>
         						</tr>
-                  <?php endif ?>
         				<?php endforeach; ?>
 
               </tbody>
@@ -58,71 +72,5 @@ $view->setVariable ("title", "Show Users");
         </div>
     </div>
 
-
-
-    <div id="background_table" class="col-4">
-      <h4 id="table_title" ><?= i18n("Trainers") ?></h4>
-      <div class="table-responsive">
-      <table id="table_color" class="table table-sm table-dark">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">#</th>
-            <th><?=i18n("Surname")?></th>
-            <th><?=i18n("Name")?></th>
-            <th><?=i18n("Operaciones")?></th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php $n=1; foreach ($users as $user): ?>
-            <?php if ($user->getIs_trainer() == 1): ?>
-
-  						<tr>
-                <td><?= $n++ ?></td>
-  							<td><?= $user->getSurname() ?></td>
-  							<td><?= $user->getName() ?></td>
-                <td>
-                  <a href="index.php?controller=users&amp;action=view&amp;id_user=<?= $user->getId_user() ?>"><span class="oi oi-magnifying-glass"></span></a>
-                  <a href="index.php?controller=users&amp;action=update&amp;id_user=<?= $user->getId_user() ?>"><span class="oi oi-loop"></span></a>
-                  <a href="index.php?controller=users&amp;action=delete&amp;id_user=<?= $user->getId_user() ?>"><span class="oi oi-trash"></span></a>
-                </td>
-  						</tr>
-            <?php endif ?>
-  				<?php endforeach; ?>
-
-        </tbody>
-      </table>
-    </div>
-    </div>
-
-
-
-
-
-    <div id="background_table" class="col-4">
-      <h4 id="table_title" ><?= i18n("Athletes") ?></h4>
-      <div class="table-responsive">
-      <table id="table_color" class="table table-sm table-dark">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">#</th>
-            <th><?=i18n("Type")?></th>
-            <th><?=i18n("Operations")?></th>
-          </tr>
-        </thead>
-        <tbody>
-  						<tr>
-                <td>1</td>
-  							<td><?=i18n("Pupils")?></td>
-                <td><a href="index.php?controller=users&amp;action=showPupils"><span class="oi oi-magnifying-glass"></span></a></td>
-  						</tr>
-              <tr>
-                <td>2</td>
-  							<td><?=i18n("Competitors")?></td>
-                <td><a href="index.php?controller=users&amp;action=showCompetitors"><span class="oi oi-magnifying-glass"></span></a></td>
-  						</tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
   </div>
 </div>

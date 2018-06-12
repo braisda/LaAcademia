06836b94bc0ee16de77b5dad2ff7014122a92277
Brais Domínguez Álvarez <braisda@gmail.com>
2018-06-12 16:36:09 +0200

    Corregido instalador

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
+echo "\n	########## INICIANDO INSTALACIÓN 'LA ACADEMIA' ##########"
+echo "\nCambiando directorio a /var/www/html..."
+cd /var/www/html
+echo "\nDescargando 'LaAcademia'...\n"
+git clone "https://github.com/braisda/LaAcademia.git"
+cd LaAcademia
+echo "\nDescarga finalizada"
+echo "\nConfigurando permisos..."
 chmod 777 -R multimedia
-exit
+echo "\nPermisos configurados correctamente"
+echo "\n"
+echo " #                   #     #####     #    ######  ####### #     # ###    #"
+echo " #         ##       # #   #     #   # #   #     # #       ##   ##  #    # #"
+echo " #        #  #     #   #  #        #   #  #     # #       # # # #  #   #   #"
+echo " #       #    #   #     # #       #     # #     # #####   #  #  #  #  #     #"
+echo " #       ######   ####### #       ####### #     # #       #     #  #  ####### "
+echo " #       #    #   #     # #     # #     # #     # #       #     #  #  #     #"
+echo " ####### #    #   #     #  #####  #     # ######  ####### #     # ### #     #"
+echo "\n	########## INSTALACIÓN FINALIZADA  ##########"
+echo "\nAcceda a la aplicación con su navegador desde: 'ip_servidor/LaAcademia'"
+echo "\n"

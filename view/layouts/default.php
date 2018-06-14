0bfddd8770933b788547cb93ecada4691237e01d
Brais Domínguez Álvarez <braisda@gmail.com>
2018-06-14 18:14:47 +0200

    Añadido favicon

--- view/layouts/default.php
+++ view/layouts/default.php
@@ -9,6 +9,9 @@ $currentuser = $view->getVariable("currentusername");
 <html>
 <head>
 
+
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
